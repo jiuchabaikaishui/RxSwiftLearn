9a1e28c1678212cc4154457bf64f54eb5c3ca59d
綦帅鹏 <qishuaipeng@qishuaipengdeiMac.local>
2019-03-05 18:33:34 +0800

    init

---
+++ RxSwiftSimple/Pods/Target Support Files/Pods-RxSwiftSimple/Pods-RxSwiftSimple-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxSwiftSimple : NSObject
+@end
+@implementation PodsDummy_Pods_RxSwiftSimple
+@end
---
+++ RxSwiftSimple/RxSwiftSimple/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSwiftSimple
+//
+//  Created by 綦帅鹏 on 2019/3/5.
+//  Copyright © 2019年 QSP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
