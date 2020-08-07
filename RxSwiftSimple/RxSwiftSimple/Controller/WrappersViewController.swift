7a5af28d7f7e68f297f902f6e5cd997a6d98bdca
QSP <QSP@163.com>
2020-08-07 18:01:48 +0800

    校准

--- RxSwiftSimple/RxSwiftSimple/Controller/WrappersViewController.swift
+++ RxSwiftSimple/RxSwiftSimple/Controller/WrappersViewController.swift
@@ -40,39 +40,12 @@ class WrappersViewController: ExampleViewController {
             self.debug("UIBarButtonItem Tapped")
         }.disposed(by: bag)
         
-        // 双向绑定
-        let value = BehaviorRelay(value: 0)
-        _ = segmentedControl.rx.value <-> value
-        value.asObservable().subscribe(onNext: { (v) in
-            self.debug("UISegmentedControl value \(v)")
-        }).disposed(by: bag)
-        
         slider.rx.value.subscribe(onNext: {
             self.debug("UISlider value \($0)")
         }).disposed(by: bag)
         
-        // 双向绑定
-        let switchV = BehaviorRelay(value: true)
-        _ = switcher.rx.value <-> switchV
-        switchV.asObservable().subscribe(onNext: {
-            self.debug("UISwitch value \($0)")
-        }).disposed(by: bag)
-        switcher.rx.value.bind(to: activityIndicator.rx.isAnimating).disposed(by: bag)
-        
         button.rx.tap.subscribe(onNext: {
             self.debug("UIButton Tapped")
-        }).disposed(by: bag)
-        
-        let textV = BehaviorRelay(value: "")
-        _ = textField.rx.textInput <-> textV
-        textV.asObservable().subscribe(onNext: {
-            self.debug("UITextField text \($0)")
-        }).disposed(by: bag)
-        
-        let textV1 = BehaviorRelay<NSAttributedString?>(value: NSAttributedString(string: ""))
-        _ = textField1.rx.attributedText <-> textV1
-        textV1.asObservable().subscribe(onNext: {
-            self.debug("UITextField attributedText \($0?.description ?? "")")
         }).disposed(by: bag)
         
         datePicker.rx.date.subscribe(onNext: {
@@ -91,6 +64,33 @@ class WrappersViewController: ExampleViewController {
             let action = UIAlertAction(title: "确定", style: .default, handler: nil)
             alter.addAction(action)
             self.present(alter, animated: true, completion: nil)
+        }).disposed(by: bag)
+        
+        // 双向绑定
+        let value = BehaviorRelay(value: 0)
+        _ = segmentedControl.rx.value <-> value
+        value.asObservable().subscribe(onNext: { (v) in
+            self.debug("UISegmentedControl value \(v)")
+        }).disposed(by: bag)
+        
+        // 双向绑定
+        let switchV = BehaviorRelay(value: true)
+        _ = switcher.rx.value <-> switchV
+        switchV.asObservable().subscribe(onNext: {
+            self.debug("UISwitch value \($0)")
+        }).disposed(by: bag)
+        switcher.rx.value.bind(to: activityIndicator.rx.isAnimating).disposed(by: bag)
+        
+        let textV = BehaviorRelay(value: "")
+        _ = textField.rx.textInput <-> textV
+        textV.asObservable().subscribe(onNext: {
+            self.debug("UITextField text \($0)")
+        }).disposed(by: bag)
+        
+        let textV1 = BehaviorRelay<NSAttributedString?>(value: NSAttributedString(string: ""))
+        _ = textField1.rx.attributedText <-> textV1
+        textV1.asObservable().subscribe(onNext: {
+            self.debug("UITextField attributedText \($0?.description ?? "")")
         }).disposed(by: bag)
         
         let textViewV = BehaviorRelay(value: "")
