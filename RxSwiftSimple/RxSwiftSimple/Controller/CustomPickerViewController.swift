9f8a8adc7b42688af870c1f73701a3f80929599b
QSP <QSP@163.com>
2020-11-30 15:59:55 +0800

    UIPickerView示例调整

--- RxSwiftSimple/RxSwiftSimple/Controller/CustomPickerViewController.swift
+++ RxSwiftSimple/RxSwiftSimple/Controller/CustomPickerViewController.swift
@@ -12,12 +12,66 @@ import RxCocoa
 
 class CustomPickerViewController: ExampleViewController {
     @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var centerPickerView: UIPickerView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        Observable.just([Array(0..<10), Array(10..<20), Array(20..<30)]).bind(to: pickerView.rx.items(adapter: PickerViewViewAdapter())).disposed(by: bag)
-        pickerView.rx.itemSelected.subscribe(onNext: { print("选中了第\($1)列第\($0)行") }).disposed(by: bag)
-        pickerView.rx.modelSelected(Int.self).subscribe(onNext: { print("选中了元素\($0)") }).disposed(by: bag)
+        if false {
+            Observable.just([Array(0..<10), Array(10..<20), Array(20..<30)])
+                .bind(to: pickerView.rx.items(adapter: SimpleSectionedPickerViewAdapter()))
+                .disposed(by: bag)
+        } else {
+            pickerView.rx
+                .items(adapter: SimpleSectionedPickerViewAdapter())(Observable.just([Array(0..<10), Array(10..<20), Array(20..<30)]))
+                .disposed(by: bag)
+        }
+        pickerView.rx.itemSelected
+            .subscribe(onNext: { print("选中了第\($1)列第\($0)行") })
+            .disposed(by: bag)
+        pickerView.rx.modelSelected(Int.self)
+            .subscribe(onNext: { print("选中了元素\($0)") })
+            .disposed(by: bag)
+        
+        let data = [Array(0..<10), Array(10..<100), Array(100..<1000)]
+        if false {
+            Observable.just(data)
+                .bind(to: centerPickerView.rx.sectionedItems, curriedArgument: ({ (_, row, component, _) in
+                    let label = UILabel()
+                    label.font = UIFont.systemFont(ofSize: 12.0)
+                    label.textAlignment = .center
+                    label.backgroundColor = UIColor.random
+                    label.text = data[component][row].description
+                    return label
+                }, { _, component in
+                    switch component {
+                    case 0:
+                        return 40.0
+                    case 1:
+                        return 80.0
+                    default:
+                        return 120.0
+                    }
+                }, { (_, _) in 50.0 }, nil, nil))
+                .disposed(by: bag)
+        } else {
+            centerPickerView.rx.sectionedItems(Observable.just(data))(({_, row, component, _ in
+                let label = UILabel()
+                label.font = UIFont.systemFont(ofSize: 12.0)
+                label.textAlignment = .center
+                label.backgroundColor = UIColor.random
+                label.text = data[component][row].description
+                return label
+            }, { (_, component) in
+                switch component {
+                case 0:
+                    return 40.0
+                case 1:
+                    return 80.0
+                default :
+                    return 120.0
+                }
+            }, { (_, _) in 50.0 }, nil, nil)).disposed(by: bag)
+        }
     }
 }
--- RxSwiftSimple/RxSwiftSimple/Models/PickerViewViewAdapter.swift
+++ RxSwiftSimple/RxSwiftSimple/Models/PickerViewViewAdapter.swift
@@ -10,9 +10,9 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-class PickerViewViewAdapter: NSObject, UIPickerViewDataSource, UIPickerViewDelegate, RxPickerViewDataSourceType, SectionedViewDataSourceType {
-    typealias Element = [[CustomStringConvertible]]
-    var items: Element = []
+class BaseSectionedPickerViewAdapter<T: CustomStringConvertible>: NSObject, UIPickerViewDataSource, RxPickerViewDataSourceType, SectionedViewDataSourceType {
+    typealias Element = [[T]]
+    fileprivate var items: Element = []
     
     func model(at indexPath: IndexPath) throws -> Any {
         items[indexPath.section][indexPath.row]
@@ -24,17 +24,81 @@ class PickerViewViewAdapter: NSObject, UIPickerViewDataSource, UIPickerViewDeleg
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         items[component].count
     }
-    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
-        let label = UILabel()
-        label.text = items[component][row].description
-        label.textAlignment = .center
-        return label
-    }
     
-    func pickerView(_ pickerView: UIPickerView, observedEvent: Event<[[CustomStringConvertible]]>) {
+    func pickerView(_ pickerView: UIPickerView, observedEvent: Event<[[T]]>) {
         Binder(self) { (adapter, items) in
             adapter.items = items
             pickerView.reloadAllComponents()
         }.on(observedEvent)
+    }
+}
+
+class SimpleSectionedPickerViewAdapter<T: CustomStringConvertible>: BaseSectionedPickerViewAdapter<T>, UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return items[component][row].description
+    }
+}
+
+class SectionedPickerViewAdapter<T: CustomStringConvertible>: BaseSectionedPickerViewAdapter<T>, UIPickerViewDelegate {
+    private var viewForRow: ((UIPickerView, Int, Int, UIView?) -> UIView)?
+    private var titleForRow: ((UIPickerView, Int, Int) -> String)?
+    private var attributedTitleForRow: ((UIPickerView, Int, Int) -> NSAttributedString)?
+    private var widthForComponent: ((UIPickerView, Int) -> CGFloat)?
+    private var heightForComponent: ((UIPickerView, Int) -> CGFloat)?
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        if let aView = viewForRow {
+            return aView(pickerView, row, component, view)
+        } else {
+            let label = UILabel()
+            label.textAlignment = .center
+            label.font = UIFont.systemFont(ofSize: 21.0)
+            if let aAttributedTitle = attributedTitleForRow {
+                label.attributedText = aAttributedTitle(pickerView, row, component)
+            }
+            else if let aTitle = titleForRow {
+                label.text = aTitle(pickerView, row, component)
+            }
+            else {
+                label.text = items[component][row].description
+            }
+            
+            return label
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+        if let width = widthForComponent {
+            return width(pickerView, component)
+        }
+        
+        return floor(UIScreen.main.bounds.width/CGFloat(items.count))
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        if let aHeight = heightForComponent {
+            return aHeight(pickerView, component)
+        }
+        
+        return 34.0
+    }
+    
+    init(viewForRow: ((UIPickerView, Int, Int, UIView?) -> UIView)?, widthForComponent: ((UIPickerView, Int) -> CGFloat)? = nil, heightForComponent: ((UIPickerView, Int) -> CGFloat)? = nil, titleForRow: ((UIPickerView, Int, Int) -> String)? = nil, attributedTitleForRow: ((UIPickerView, Int, Int) -> NSAttributedString)? = nil) {
+        super.init()
+        self.viewForRow = viewForRow
+        self.widthForComponent = widthForComponent
+        self.heightForComponent = heightForComponent
+    }
+}
+
+extension Reactive where Base: UIPickerView {
+    func sectionedItems<T: CustomStringConvertible>
+        (_ source: Observable<[[T]]>)
+    -> ((viewForRow: (UIPickerView, Int, Int, UIView?) -> UIView, widthForComponent: ((UIPickerView, Int) -> CGFloat)?, heightForComponent: ((UIPickerView, Int) -> CGFloat)?, titleForRow:((UIPickerView, Int, Int) -> String)?, attributedTitleForRow: ((UIPickerView, Int, Int) -> NSAttributedString)?))
+    -> Disposable {
+        return { arg in
+            let adapter = SectionedPickerViewAdapter<T>(viewForRow: arg.0, widthForComponent: arg.1, heightForComponent: arg.2, titleForRow: arg.3, attributedTitleForRow: arg.4)
+            return items(adapter: adapter)(source)
+        }
     }
 }
